80eb152c1433f51c89252979154c6c7d673bc250
Uday Jangra <85983169+Udayjangra76@users.noreply.github.com>
2023-06-17 00:12:53 +0530

    Update useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -7,26 +7,26 @@ const useFetch = (url) => {
     let [data, setData] = useState([]);
     let [loading, setLoading] = useState(false);
     let [error, setError] = useState(false);
-
-   useEffect(() => {
         
-  useEffect(() => {
-        const fetchData = async () => {
-            setLoading(true);
-            const newRequest = axios.create({
-                baseURL: `${BASE_URL}/api/`,
-                withCredentials: true,
-            });
-            newRequest.get(url).then(response => {
-                setData(response.data);
-            }).catch(err => {
-                setError(err);
-            });
-            setLoading(false);
-        }
-        fetchData();
-    }, [url])
+  const fetchData = async () => {
+        setLoading(true);
+        const newRequest = axios.create({
+            baseURL: `${BASE_URL}/api/`,
+            withCredentials: true,
+        });
+        newRequest.get(url).then(response => {
+            setData(response.data);
+        }).catch(err => {
+            setError(err);
+        });
+        setLoading(false);
+    }
 
+    useEffect(() => {
+        fetchData();
+        // eslint-disable-next-line
+    }, []);
+    
     const reFetch = async () => {
         setLoading = true;
         const newRequest = axios.create({
